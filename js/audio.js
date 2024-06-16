41556e487684f18946ec3b1e5d98f461ce3d2f97
melodicht <marvincht@gmail.com>
2024-06-16 16:16:26 +0800

    Add building_denied SFX

--- js/audio.js
+++ js/audio.js
@@ -18,7 +18,8 @@ const soundFolderRoot = "sound/";
 // the file (including the file itself) relative to `soundFolderRoot`. That is,
 // you do not have to write `soundFolderRoot` in the file path.
 const nameToFilePath = [
-  ["building_built", "building_built.mp3"]
+  ["building_built", "building_built.mp3"],
+  ["building_denied", "building_denied.mp3"],
 ];
 
 const nameToAudio = new Map(
