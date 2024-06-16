9e986a8a4ccb02c6dba94b36f4dd448abeb8cf6f
melodicht <marvincht@gmail.com>
2024-06-16 12:11:57 +0800

    Add world builder's tile type to tile and society populator
    Lgoci

--- js/logic.js
+++ js/logic.js
@@ -46,6 +46,9 @@
 // A TileType is one of the following.
 // Represents the type of tile. Note that no tile on the map should have a type
 // of unassigned.
+// Do not simply change the numnbers of a TILE_TYPE, because the world builder
+// uses integers to build the world (for convenience). If you do need to change
+// the numbers, please also update the world builder initial tiles.
 const TILE_TYPE = Object.freeze({
   UNASSIGNED: 0,
   WALL: 1,
@@ -68,12 +71,8 @@ const TILE_TYPE = Object.freeze({
 // - Capital
 // - EnemyCamp
 // Represents the type of tile that it is.
-// NOTE(marvin):
-// As of now, all derived tiles can only make a shallow copy of this
-// tile. structuredClone(), which is used to make deep copies does not work with
-// objects with methods defined. Currently, the abstract base tile does not
-// contain any fields with itself are objects that are require cloning. Thus,
-// shallow cloning will suffice.
+// NOTE(marvin): Use _.cloneDeep(...) to clone. structuredClone doesn't work
+// with objects with methods.
 const ATILE = {
   tag: TILE_TYPE.UNASSIGNED,  // Must be replaced by derived tile
   society: new Map(),         // Must be replaced by derived tile
@@ -147,12 +146,14 @@ if (tile.hasOwnProperty("pathUnitsQueues")) {
 // - SOLDIER: 1
 // - WALKER: 2
 // - QUEEN: 3
+// - ATTACKER: 4
 // Represents a way to categorize units based on what they do.
 const ROLE = Object.freeze({
   FARMER: 0,
   SOLDIER: 1,
   WALKER: 2,
   QUEEN: 3,
+  ATTACKER: 4,
 });
 
 // A Wall is an ATile that has no society.
@@ -171,15 +172,6 @@ const WALKABLE_TILE_PREFAB = {
     [ROLE.ATTACKER, {capacity: Infinity, units: []}],
   ])
 }
-
-// A Unit is a {energy: Integer, affiliation: Affiliation, pathId: Integer,
-// indexInPath: Integer, oneOffPath: [U OneOffPath false], direction: Direction, isCarryingFood: Boolean,
-// hasMovedInTick: Boolean, isSelected: Boolean, pos: Pos }
-// Represents a unit that is moving to a particular direction or not moving, and
-// may or may not be carrying food. pathId and indexInPath are both -1 if the
-// unit is not associated with any paths. The pos must be initialized along with
-// the unit's position in the world grid, and continue to remain in sync.
-
 
 // A Affiliation is one of:
 // - YOURS: 0,
@@ -201,6 +193,26 @@ const DIRECTION = Object.freeze({
   FROM: 1,
   STATIONARY: 2,
 });
+
+// A Unit is a {energy: Integer, affiliation: Affiliation, pathId: Integer,
+// indexInPath: Integer, oneOffPath: [U OneOffPath false], direction: Direction, isCarryingFood: Boolean,
+// hasMovedInTick: Boolean, isSelected: Boolean, pos: Pos }
+// Represents a unit that is moving to a particular direction or not moving, and
+// may or may not be carrying food. pathId and indexInPath are both -1 if the
+// unit is not associated with any paths. The pos must be initialized along with
+// the unit's position in the world grid, and continue to remain in sync.
+
+const UNIT_PREFAB = {
+  energy: 100,
+  affiliation: AFFILIATION.YOURS,
+  pathId: null,
+  indexInPath: 0,
+  oneOffPath: false,
+  direction: DIRECTION.TO,
+  isCarryingFood: false,
+  hasMovedInTick: false,
+  isSelected: false,
+}
 
 // A FoodStorage is a {tag: TileType, foodStored: Integer,
 // pathUnitsQueues: [Array-of PathUnitsQueue]}
@@ -240,6 +252,7 @@ const CAPITAL_PREFAB = {
   tag: TILE_TYPE.CAPITAL,
   society: new Map([[ROLE.SOLDIER, {capacty: 20, units: []}],
 		    [ROLE.ATTACKER, {capacity: 20, units: []}]]),
+  eggTimeGroups: [{ticksPassed: 2, eggs: 5}, {ticksPassed: 5, eggs: 10}],
 }
 
 // An EggTimeGroup is a {ticksPassed: Integer, eggs: Integer}
@@ -383,6 +396,165 @@ const initialWorld = {
     [wall, wall, foodStorage, wall],
   ],
   paths: paths0,
+  buildTileOptions: buildTileOptions0,
+  buildTileSelected: null,
+  clickMode: CLICK_MODE.INFO,
+  selectedUnits: [],
+}
+
+// WORLD BUILDER (MORE CONVENIENT)
+
+// TODO(marvin):
+// However, currently has no way of adding paths. I can do that after reworking
+// the path system so that the one off and cyclic paths aren't weird.
+
+// Uses TILE_TYPE. For convenience, we write down integers instead of
+// TILE_TYPE.X.
+// The length of initialTileTypes must be a multiple of initialTileTypesRowLength.
+const initialTileTypesRowLength = 5;  // Number of columns.
+const initialTileTypes = [
+  1, 1, 1, 1, 1,
+  1, 1, 5, 1, 1,
+  1, 1, 2, 1, 1,
+  1, 1, 2, 1, 1,
+  1, 1, 2, 1, 1,
+  4, 2, 2, 2, 3,
+  1, 1, 6, 1, 1,
+];
+
+if (initialTileTypes.length % initialTileTypesRowLength != 0) {
+  console.error("The length of initialTileTypes must be a multiple of initialTileTypesRowLength.");
+}
+
+// Must follow the form [Nat, Nat, [Role, Nat, ...], ...]
+// which corresponds to [row position, col position, [role, number of units in
+// that row, ...], ...]
+// Note that row and col position counts from 0. Row counts from top to bottom,
+// and col counts from left to right.
+const initialPosToSociety = [
+  5, 0, [ROLE.SOLDIER, 10, ROLE.FARMER, 5, ROLE.ATTACKER, 15],
+  5, 4, [ROLE.SOLDIER, 15, ROLE.ATTACKER, 2],
+  1, 2, [ROLE.SOLDIER, 10, ROLE.ATTACKER, 2],
+  6, 2, [ROLE.SOLDIER, 5, ROLE.ATTACKER, 15],
+  3, 2, [ROLE.WALKER, 1, ROLE.ATTACKER, 1],
+  3, 2, [ROLE.WALKER, 1, ROLE.ATTACKER, 1],
+];
+
+if (initialPosToSociety.length % 3 != 0) {
+  console.error("initialPosToSociety should have a length that is a multiple of 3");
+}
+
+// [2D-Arrayof Tile], [Flat-Listof Nat Nat [Flat-Listof Role Nat]] -> Void
+// The given society takes a special form (assume that is true), one that is more convenient for
+// writing out, but we will have to parse to its canonical form. The given tiles
+// is mutated directly, and the tiles are returned again for compositionality.
+function _addSocietyToInitialTiles(tiles, society) {
+  let currItem = [];
+  let currItemLength = 0;
+
+  for (const societyElement of society) {
+    currItem.push(societyElement);
+    currItemLength++;
+
+    if (currItemLength >= 3) {
+      // currItem should have the form [Nat, Nat, [Role, Nat, ...]]
+      const r = currItem[0];
+      const c = currItem[1];
+      const rolesCounts = currItem[2];
+
+      const tileToModify = tiles[r][c];
+      _addSocietyToTile(tileToModify, rolesCounts);
+
+      currItem = []
+      currItemLength = 0;
+    }
+  }
+  
+  return tiles;
+
+  // Tile, [Role, Nat, ...] -> Void
+  // Mutates the given tile with the rolesCounts.
+  function _addSocietyToTile(tile, rolesCounts) {
+    let currItem = [];
+    let currItemLength = 0;
+
+    for (const rolesCountsItem of rolesCounts) {
+      currItem.push(rolesCountsItem);
+      currItemLength++;
+
+      if (currItemLength >= 2) {
+	const role = currItem[0];
+	const count = currItem[1];
+
+	const societyRole = tile.society.get(role);
+	for (let c = 0; c < count; c++) {
+	  societyRole.units.push(_.cloneDeep(UNIT_PREFAB));
+	}
+
+	currItem = [];
+	currItemLength = 0;
+      }
+    }
+
+    if (currItemLength > 0) {
+      console.error("rolesCounts is not well-formed, it should have length divisible by 2.");
+    }
+  }
+}
+
+// [Listof TileType] -> [2D-Arrayof Tile]
+function _initialTileTypesToTiles(tileTypes) {
+  let rv = [];
+  let currRow = [];
+  let currRowLength = 0;
+  
+  for (const tileType of tileTypes) {
+    currRow.push(_tileTypeToDefaultTile(tileType));
+    currRowLength++;
+    
+    if (currRowLength >= initialTileTypesRowLength) {
+      rv.push(currRow);
+      currRow = [];  // Assign to new object, not mutate.
+      currRowLength = 0;
+    }
+  }
+
+  return rv;
+
+  // TileType -> Tile
+  // Given a tile type, returns its corresponding tile which is a cloned object
+  // of its prefab.
+  function _tileTypeToDefaultTile(tileType) {
+    return _.cloneDeep(_tileTypeToPrefab(tileType));
+
+    // Returns the corresponding prefab of the given tile type, serves as a helper
+    function _tileTypeToPrefab(tileType) {
+      switch (tileType) {
+      case TILE_TYPE.UNASSIGNED:
+	console.error("Cannot use TILE_TYPE.UNASSIGNED in world builder, using TILE_TYPE.WALL instead.");
+	return WALL_PREFAB;
+      case TILE_TYPE.WALL:
+	return WALL_PREFAB;
+      case TILE_TYPE.WALKABLE_TILE:
+	return WALKABLE_TILE_PREFAB;
+      case TILE_TYPE.FOOD_STORAGE:
+	return FOOD_STORAGE_PREFAB;
+      case TILE_TYPE.FOOD_FARM:
+	return FOOD_FARM_PREFAB;
+      case TILE_TYPE.CAPITAL:
+	return CAPITAL_PREFAB;
+      case TILE_TYPE.ENEMY_CAMP:
+	return ENEMY_CAMP_PREFAB;
+      }
+    }
+  }
+
+  
+}
+
+const initialWorldWithBuilder = {
+  grid: _addSocietyToInitialTiles(_initialTileTypesToTiles(initialTileTypes), initialPosToSociety),
+  paths: [],
   buildTileOptions: buildTileOptions0,
   buildTileSelected: null,
   clickMode: CLICK_MODE.INFO,
@@ -865,6 +1037,14 @@ function _onTickUnitInPaths(unit, worldGrid, worldPaths) {
   // Moves the given unit one step forward in the given path from its given index
   // with the given direction, if it is moving.
   if (unit.hasMovedInTick) {
+    return;
+  }
+
+  // TODO: pathId being null is just a quick and easy way to get units in
+  // walkable tiles that aren't moving... Though it doesn't really make sense
+  // why units with the role "walker" aren't walking. There's got to be a better
+  // way to model them.
+  if (!unit.isOneOffPath && unit.pathId == null) {
     return;
   }
 
